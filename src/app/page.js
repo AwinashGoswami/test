b36dc70431bc340b8fa7610fd9faa1eb02fef1d5
awinash <awinashgoswami3@gmail.com>
2024-02-24 12:58:50 +0500

    data from api

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,40 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
+import { useEffect, useState } from "react";
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
+const Posts = () => {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+                if (response.ok) {
+                    const data = await response.json();
+                    setPosts(data);
+                } else {
+                    console.error(`Error: Unable to fetch data. Status code: ${response.status}`);
+                }
+            } catch (error) {
+                console.error(`Error: ${error.message}`);
+            }
+        };
+        fetchData();
+    }, []);
+
+    return (
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            <h1>Posts</h1>
+            <ul>
+                {posts.map((post) => (
+                    <li key={post.id}>
+                        <a href={`/Posts/${post.id}`}>
+                            {post.title}
+                        </a>
+                    </li>
+                ))}
+            </ul>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    );
 }
+
+export default Posts;
---
+++ src/pages/Posts/index.js
@@ -0,0 +1,23 @@
+// // pages/posts/[postId].js
+// import { useRouter } from 'next/router';
+// import { useEffect } from 'react';
+
+// const PostDetail = () => {
+//     const router = useRouter();
+//     const { postId } = router.query;
+
+//     // Fetcuseh post details using postId and render them
+// useEffect(()=>{
+//   console.log(postId,'postId')
+// })
+//     return (
+//         <div>
+//             <h2>Post Detail</h2>
+//             <p>Post ID: {postId}</p>
+//             {postId.title}
+//             {/* Render other details of the post */}
+//         </div>
+//     );
+// };
+
+// export default PostDetail;
